8423dcebe42f5e653c9402886a62b7a7932a4ebc
Biswajit Ghosal <161633802+dev-biswajitghosal@users.noreply.github.com>
2024-03-12 17:00:18 +0530

    init

--- app.py
+++ app.py
@@ -1,18 +1,28 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+from gemini import generate_content
 
 app = Flask(__name__)
 CORS(app)
 
 
-@app.route('/', methods=['GET'])
-def index():
-    return jsonify({'message': 'Hello, World!'})
-
-
-@app.route('/api', methods=['GET'])
+@app.route('/api/auth', methods=['POST', 'GET'])
 def api():
-    return jsonify({'message': 'Hello, World!'})
+    if request.method == 'POST':
+        if 'Authorization' in request.headers:
+            auth_token = request.headers['Authorization']
+            print("Authorization token:", auth_token)
+        else:
+            return jsonify({'message': 'Authorization token not found.'}), 401
+        request_data = request.get_json()
+        prompt = request_data.get('prompt')
+        print(prompt)
+        response = generate_content(prompt)
+        print(response)
+        return jsonify({'response': response})
+    else:
+        return jsonify({"message": 'Please Provide Auth Key'})
 
 
-app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)

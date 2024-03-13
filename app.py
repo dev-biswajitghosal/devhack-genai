e186ebc302f841695319ebf56d6decfe6a994436
Biswajit Ghosal <161633802+dev-biswajitghosal@users.noreply.github.com>
2024-03-14 00:57:26 +0530

    added API KEY for additional security

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
 from gemini import generate_content
+from auth import authenticate
 
 app = Flask(__name__)
 CORS(app)
@@ -14,31 +15,38 @@ def index():
 
 
 @app.route('/api/get_healthcare_data', methods=['POST'])
-def api():
-    zip_code = request.form.get('zip')
-    prompt = f"Find healthcare providers near {zip_code}"
-    response = generate_content(prompt)
-    return jsonify({'response': response})
+def get_healthcare_data():
+    if 'Authorization' in request.headers:
+        auth_token = request.headers['Authorization']
+        is_authenticated = authenticate(auth_token)
+        if not is_authenticated:
+            return jsonify({'message': 'Please Give Correct API Key'}), 401
+        else:
+            zip_code = request.form.get('zip')
+            prompt = f"Find healthcare providers near {zip_code}"
+            response = generate_content(prompt)
+            return jsonify({'response': response}), 200
+    else:
+        return jsonify({'message': 'Please Give Correct API Key'}), 401
 
 
 @app.route('/api/analyze_risk_profile', methods=['POST'])
-def generate():
-    name = request.form.get('name')
-    industry = request.form.get('industry')
-    location = request.form.get('location')
-    age = request.form.get('age')
-    prompt = f"Analyze the risk profile for {name} in the {industry} industry in {location} at the age of {age}"
-    response = generate_content(prompt)
-    return jsonify({'response': response})
-
-
-@app.route('/api/auth', methods=['POST'])
-def auth():
+def analyze_risk_profile():
     if 'Authorization' in request.headers:
         auth_token = request.headers['Authorization']
-        print("Authorization token:", auth_token)
+        is_authenticated = authenticate(auth_token)
+        if not is_authenticated:
+            return jsonify({'message': 'Please Give Correct API Key'}), 401
+        else:
+            name = request.form.get('name')
+            industry = request.form.get('industry')
+            location = request.form.get('zip')
+            age = request.form.get('age')
+            prompt = f"Analyze the risk profile for {name} in the {industry} industry in {location} at the age of {age}"
+            response = generate_content(prompt)
+            return jsonify({'response': response}), 200
     else:
-        return jsonify({'message': 'Authorization token not found.'}, 401)
+        return jsonify({'message': 'Please Give Correct API Key'}, 401)
 
 
 if __name__ == '__main__':
